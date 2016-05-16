8e40bc53c4504a1475ce572b5296582826fd9b49
juku@tmk.edu.ee <juku@tmk.edu.ee>
2016-05-16 23:52:11 +0300

    Kodust 2

---
+++ ylesanded/yl0101.php
@@ -0,0 +1,16 @@
+================================
+Ülesanne 0101
+
+Järgnevas ülesandes tohib kasutada vaid IF tingimust ja FOR tsükklit. Arvude üleühe kuvamiseks kasutage lisamuutujat. Kuvada ühe FOR tsükkliga välja järgmine arvude jada järgmisel kujul: 
+
+1
+3
+5
+7
+9
+11
+13
+15
+17
+19
+
---
+++ ylesanded/yl0103.php
@@ -0,0 +1,15 @@
+================================
+Ülesanne 0103
+
+Järgnevas ülesandes tohib kasutada vaid IF tingimust, mooduli võtmist (mod %) ja FOR tsükklit. Kuvada ühe FOR tsükkliga välja järgmine arvude jada järgmisel kujul: 
+
+100
+200
+300
+400
+500
+600
+700
+800
+900
+1000
